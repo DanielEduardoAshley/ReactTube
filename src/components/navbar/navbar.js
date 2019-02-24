626363a8540a7a8be16786ca70d8639dcf562518
Srirupa <srirupadatta@pursuit.org>
2019-02-24 12:25:41 -0500

    imported CSS, added classNames, and set placeholder for navbar logo

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css';
 
 class Navbar extends Component {
   render() {
     return (
         <>
-            <div>
-                <Link to='/home'>Home</Link>
-                <Link to='/user'>User</Link>
-                <Link to='/feededitor'>Feed Editor</Link>
+            <div className="header">
+                <div className="navbar">
+                    <p>PRYD</p>
+                    <Link to='/home'>Home</Link>
+                    <Link to='/user'>User</Link>
+                    <Link to='/feededitor'>Feed Editor</Link>
+                </div>
 
-                <input placeholder='Search'></input>
-                <Link to='/search/:search_query'>Search</Link>
+                <div className="searchBox">
+                    <input placeholder='Search'></input>
+                    <Link to='/search/:search_query'>Search</Link>
+                </div>
             </div>
         </>
     );
