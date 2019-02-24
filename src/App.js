35c5b8c5493f6e22aa050f96bd8a16cbbe00890d
daniel ashley <danielashley@pursuit.org>
2019-02-24 12:12:46 -0500

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,72 @@ const Placeholder = (props) => {
 }
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      videoArray : []
+    }
+  }
+
+
+   VideoPlayer = ({ id }) => {
+    const link = `https://www.youtube.com/embed/${id}?autoplay=1&fs=1&origin=http://localhost:3001`;
+  
+    return (
+      <iframe title='yt-video' type="text/html" width="640" height="360"
+    src={link} frameBorder="0"></iframe>
+    )
+    }
+
+
+  componentDidMount(){
+    axios({
+      method: 'get',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      params: {
+        part: 'snippet',
+        maxResults: 8,
+        videoDefinition: 'high',
+        type: 'video',
+        videoEmbeddable: 'true',
+        key: 'AIzaSyDk4Baz4ZsCIIY-zwzjEgOATbmVwjZVVpc',
+        q: 'orochimaru',
+        pageToken: ''
+      }
+    }).then(response=>{
+      console.log(response)
+      response.data.items.map(e=>{
+        return this.setState({
+          videoArray : (this.videoArray || []).concat(e.id.videoId) 
+        })
+      })
+      console.log( this.state.videoArray)
+  
+      
+        
+      
+    })
+  }
+
+
+
+
+
+
+
   render() {
     return (
       <div>
         <h1>React Tube</h1>
+{/* ////////Daniel's Stuff */}
+
+
+
+
+
+
+
+
 
         <Navbar />
         
