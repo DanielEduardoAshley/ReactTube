e5adf4c087d1967d8ecc8a02f0ea7dd42ca0c0a2
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-23 21:12:00 -0500

    Wrote all the paths and passed indummy placeholder component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
+
+const Placeholder = (props) => {
+  return (
+      <h1>This is the placeholder for our components</h1>
+  );
+}
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>React Tube</h1>
+
+        <Navbar />
+        
+        <Route path='/' exact component={Placeholder}/>
+        <Route path='/home' exact component={Placeholder}/>
+        <Route path='/video/:video_id' exact component={Placeholder}/>
+        <Route path='/search/:search_query' exact component={Placeholder}/>
+        <Route path='/user' exact component={Placeholder}/>
+        <Route path='/feededitor' exact component={Placeholder}/>
+
+        {/* <Route path='/user/:user_id/history' exact component={Placeholder}/> */}
       </div>
     );
   }
