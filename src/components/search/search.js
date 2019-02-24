9aa595546cad9d0872f57b58c10f14ae79dfbae9
Srirupa <srirupadatta@pursuit.org>
2019-02-24 14:26:26 -0500

    created initial Search Component

---
+++ src/components/search/search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+render() {
+    return (
+        <div>
+            <p>Search Result Page</p>
+        </div>
+    )
+}
+}
+
+export default Search;
