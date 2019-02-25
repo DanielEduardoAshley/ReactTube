544f57e2ff2b02a5028387d8aab36d28dba859a3
daniel ashley <danielashley@pursuit.org>
2019-02-24 21:16:56 -0500

    App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ const Placeholder = (props) => {
 
 class App extends Component {
 
-
    /* 
         Daniel's workspace
          */
@@ -30,120 +29,11 @@ class App extends Component {
       videoArray : []
     }
   }
-
-
-   VideoPlayer = ({ id }) => {
-    const link = `https://www.youtube.com/embed/${id}?autoplay=1&fs=1&origin=http://localhost:3001`;
-  
-    return (
-      <iframe title='yt-video' type="text/html" width="640" height="360"
-    src={link} frameBorder="0"></iframe>
-    )
-    }
-
-
-  componentDidMount(){
-    axios({
-      method: 'get',
-      url: 'https://www.googleapis.com/youtube/v3/search',
-      params: {
-        part: 'snippet',
-        maxResults: 8,
-        videoDefinition: 'high',
-        type: 'video',
-        videoEmbeddable: 'true',
-        key: 'AIzaSyDk4Baz4ZsCIIY-zwzjEgOATbmVwjZVVpc',
-        q: 'orochimaru',
-        pageToken: ''
-      }
-    }).then(response=>{
-      console.log(response)
-      return response.data.items.map(e=>{
-        console.log(e.id.videoId)
-
-       return  this.setState({
-          videoArray : (this.state.videoArray || []).concat([e.id.videoId]) 
-        })
-      })
-  
-      
-        
-      
-    })
-    .then(()=>{
-     const arr = []
-    console.log(this.state.videoArray)
-     this.state.videoArray.map((e)=>{
-      return axios({
-        method: 'get',
-        url: 'https://www.googleapis.com/youtube/v3/videos',
-        params: {
-          part: 'id,snippet,statistics',
-          key: 'AIzaSyDk4Baz4ZsCIIY-zwzjEgOATbmVwjZVVpc',
-          id: e,
-        }
-      }).then((response)=>{
-        arr.push(
-          
-          response.data.items[0].id)
-           
-              // obj.response.data.items[0].id
-	      // snippet.publishedAt,
-	      // snippet.channelId,
-	      // snippet.title,
-	      // snippet.description,
-	      // thumbnails.maxres.url,
-	      // thumbnails.maxres.width,
-	      // thumbnails.maxres.url.height,
-	      // channelTitle,
-	      // tags,
-	      // statistics.viewCount,
-	      // statistics.likeCount,
-	      // statistics.dislikeCount,
-	      // statistics.favoriteCount,
-	      // statistics.commentCount,
-      
-        console.log(arr)
-
-      })
-      
-      
-    })
-    })
-       
-            
-
-      }
-
-        
-
-
-      
-
-
-
-   /* 
-        Daniel's workspace
-         */
-
-
-
-
-
-
-
   render() {
   //  const id = this.state.videoArray[0]
     return (
       <div>
         <h1>React Tube</h1>
-
-
-
-
-
-
-
 
 
         <Navbar />
