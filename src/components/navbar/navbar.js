8c4711fc1d1fc1b69c89c2ce195e601d79ac4175
daniel ashley <danielashley@pursuit.org>
2019-02-24 21:03:42 -0500

    merging

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
+<<<<<<< danBranch
   render() {
     return (
         <>
@@ -16,6 +17,46 @@ class Navbar extends Component {
         </>
     );
   }
+=======
+    state = {
+        searchInput: '',
+    }
+
+    handleClick = (e) => {
+        this.props.history.push(`/search/${this.state.searchInput}`);
+    }
+
+    onChange = (e) => {
+        this.setState({searchInput: e.target.value})
+    }
+
+    onKeyDown = (e) => {
+        if(e.key.toLowerCase() === 'enter'){
+            this.setState({searchInput: e.target.value})
+        }
+    }
+
+    render() {
+
+        return (
+            <>
+                <div className="header">
+                    <div className="navbar">
+                        <p className="navButton">PRYD</p>
+                        <Link to='/' className="navButton">Home</Link>
+                        <Link to='/user' className="navButton">User</Link>
+                        <Link to='/feededitor' className="navButton" >Feed Editor</Link>
+                    </div>
+                    <div className="searchBox">
+                        <input placeholder='Search' className='navInput' onChange={this.onChange} onKeyDown={this.onKeyDown}></input>
+                        <button className="searchButton" onClick={this.handleClick}>Search</button>
+                    </div>
+                        
+                    </div>
+            </>
+        );
+    }
+>>>>>>> local
 }
 
 export default Navbar;
