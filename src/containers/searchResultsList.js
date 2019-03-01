17c397e0a9ab0ae436ace86c74e661ad2cab1aa8
Srirupa <srirupadatta@pursuit.org>
2019-03-01 00:51:19 -0500

    removed commented out code

--- src/containers/searchResultsList.js
+++ src/containers/searchResultsList.js
@@ -5,33 +5,20 @@ import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reac
 const onClick = ({id, fun}) => {
     // const {id, fun} = obj;
     const {videoId} = id;
-    // console.log('here in onclick', obj, id)
-    
-    // console.log('This is the videoId before API call', videoId)
-    // axiosSecondCall(videoId).then((res)=> {
-    //     let vidID = '';
-    //     console.log('This is an array we gonna loop thru it and find id',res.data.items)
-    //     return res.data.items.map((e,i) => {
-    //         const { id } = e.id;
-    //         console.log('here is id',id)
-    //         return id;
-    //     })
-    // })
-    // console.log('2nd call', axiosSecondCall(videoID))
-    // .then(())
+
     fun(`/video/${videoId}`);
 }
 
 const SearchResultsList = ({results, pop}) => {
     console.log('Is this is props?',pop.history.push)
     const fun = pop.history.push;
-    // console.log('PROPS', props)
     let str = ''
     // console.log('resultsss', props.results)
 
 
     return (
     <>
+        
         <div className='rowsss'>
             {
                 results.map((e, i) => {
