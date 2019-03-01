ced9ab70f948d4301bb2b87c824c156e12eab258
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-28 23:26:38 -0500

    added enter functionality and messed with styling

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,51 +1,23 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import axiosFirstCall from '../../services/axios';
-import Moment from 'moment';
 import './navbar.css';
-
 
 class Navbar extends Component {
     state = {
         searchInput: '',
     }
 
-    handleClick = (e) => {
+    handleClick = () => {
         this.props.history.push(`/search/${this.state.searchInput}`);
-
-        // axiosFirstCall(this.state.searchInput)
-        //     .then((res) => {
-        //         console.log('response', res)
-        //         const resultsArr = [];
-        //         res.data.items.map((e, i) => {
-        //             const { id, snippet } = e;
-        //             const { videoId } = id;
-        //             const { publishedAt, channelTitle, channelId, description, thumbnails, title } = snippet;
-        //             const thumbnailsURL = thumbnails.high.url;
-        //             const published = Moment(`${publishedAt}`, "YYYYMMDD").fromNow();
-        //             const resultsInfo = { id, snippet, videoId, published, channelTitle, channelId, description, thumbnailsURL }
-
-        //             return resultsArr.push(resultsInfo);
-        //         });
-        //         return resultsArr;
-        //     })
-        //     .then(
-        //         (results) => {
-        //             this.setState({
-        //                 prevSearch: this.state.prevSearch.concat(this.state.results),
-        //                 currentResults: results,
-        //             }, () => console.log('my state', this.state))
-        //         })
-        //     .catch((err) => console.log(err));
     }
 
     onChange = (e) => {
         this.setState({searchInput: e.target.value})
     }
 
-    onKeyDown = (e) => {
+    handleEnter = (e) => {
         if(e.key.toLowerCase() === 'enter'){
-            this.setState({searchInput: e.target.value})
+            this.handleClick()
         }
     }
 
@@ -53,19 +25,18 @@ class Navbar extends Component {
 
         return (
             <>
-                <div className="header">
-                    <div className="navbar">
-                        <p className="navButton">PRYD</p>
-                        <Link to='/home' className="navButton">Home</Link>
-                        <Link to='/user' className="navButton">User</Link>
-                        <Link to='/feededitor' className="navButton" >Feed Editor</Link>
-                    </div>
-                    <div className="searchBox">
-                        <input placeholder='Search' className='navInput' onChange={this.onChange} onKeyDown={this.onKeyDown}></input>
-                        <button className="searchButton" onClick={this.handleClick}>Search</button>
-                    </div> 
-                    {/* Fix Functionality    */}
-                    </div>
+            <div className='header' style={{ display:'flex', flexWrap:'wrap', paddingLeft:'30px', marginTop:'50px;'}}>
+                <div className='navbar' style={{display:'flex', flexWrap:'wrap', marginRight:'30px'}}>
+                    <p className="navButton">PRYD</p>
+                    <Link to='/home' className="navButton">Home</Link>
+                    <Link to='/user' className="navButton">User</Link>
+                    <Link to='/feededitor' className="navButton">Feed Editor</Link>
+                </div>
+                <div className='navbarSearch'>
+                    <input placeholder='Search' className='navInput' onChange={this.onChange} onKeyDown={this.handleEnter}></input>
+                    <p className="searchButton" onClick={this.handleClick}>Search</p>
+                </div>
+            </div>
             </>
         );
   }
