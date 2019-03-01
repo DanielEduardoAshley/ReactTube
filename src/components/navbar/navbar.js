85950a7e4b20f62d8748994182237ae6c1ded722
Srirupa <srirupadatta@pursuit.org>
2019-03-01 00:54:47 -0500

    inserted 2 ternary operators into jsx

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,56 +1,63 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import axiosFirstCall from '../../services/axios';
+import { axiosFirstCall } from '../../services/axios';
 import Moment from 'moment';
 import './navbar.css';
+import SearchResultsList from '../../containers/searchResultsList';
 
 
 class Navbar extends Component {
     state = {
         searchInput: '',
+        currentResults: [],
     }
 
     handleClick = (e) => {
         this.props.history.push(`/search/${this.state.searchInput}`);
+        console.log('input', this.state.searchInput)
+        console.log('???', this.props)
+        axiosFirstCall(this.state.searchInput)
+            .then((res) => {
+                console.log('response', res)
+                const resultsArr = [];
+                res.data.items.map((e, i) => {
+                    const { id, snippet } = e;
+                    const { videoId } = id;
+                    const { publishedAt, channelTitle, channelId, description, thumbnails, title } = snippet;
+                    const { high } = thumbnails;
+                    const { url } = high;
+                    const published = Moment(`${publishedAt}`, "YYYYMMDD").fromNow();
+                    const resultsInfo = { id, snippet, videoId, published, channelTitle, channelId, description, thumbnails, title, high, url }
 
-        // axiosFirstCall(this.state.searchInput)
-        //     .then((res) => {
-        //         console.log('response', res)
-        //         const resultsArr = [];
-        //         res.data.items.map((e, i) => {
-        //             const { id, snippet } = e;
-        //             const { videoId } = id;
-        //             const { publishedAt, channelTitle, channelId, description, thumbnails, title } = snippet;
-        //             const thumbnailsURL = thumbnails.high.url;
-        //             const published = Moment(`${publishedAt}`, "YYYYMMDD").fromNow();
-        //             const resultsInfo = { id, snippet, videoId, published, channelTitle, channelId, description, thumbnailsURL }
-
-        //             return resultsArr.push(resultsInfo);
-        //         });
-        //         return resultsArr;
-        //     })
-        //     .then(
-        //         (results) => {
-        //             this.setState({
-        //                 prevSearch: this.state.prevSearch.concat(this.state.results),
-        //                 currentResults: results,
-        //             }, () => console.log('my state', this.state))
-        //         })
-        //     .catch((err) => console.log(err));
+                    return resultsArr.push(resultsInfo);
+                });
+                return resultsArr;
+            })
+            .then(
+                (results) => {
+                    this.setState({
+                        prevSearch: (this.state.prevSearch || []).concat(this.state.results),
+                        currentResults: results,
+                    }, () => console.log('my state', this.state))
+                })
+            .catch((err) => console.log(err));
     }
 
     onChange = (e) => {
-        this.setState({searchInput: e.target.value})
+        this.setState({ searchInput: e.target.value })
     }
 
     onKeyDown = (e) => {
-        if(e.key.toLowerCase() === 'enter'){
-            this.setState({searchInput: e.target.value})
+        if (e.key.toLowerCase() === 'enter') {
+            this.setState({ searchInput: e.target.value })
         }
     }
 
     render() {
-
+        const aid = { ...this.props }
+        let list = this.props.location.pathname.split('/')
+        console.log('LIST', list)
+        console.log('THIS', this.props)
         return (
             <>
                 <div className="header">
@@ -63,12 +70,19 @@ class Navbar extends Component {
                     <div className="searchBox">
                         <input placeholder='Search' className='navInput' onChange={this.onChange} onKeyDown={this.onKeyDown}></input>
                         <button className="searchButton" onClick={this.handleClick}>Search</button>
-                    </div> 
-                    {/* Fix Functionality    */}
                     </div>
+                    <div className='searchTitle'>
+                        { list[1] === 'search' ? <p>Search Results for {this.state.searchInput}</p>
+ : null}
+                    </div>
+                    <div>
+                        {list[1] === 'search' ? <SearchResultsList pop={aid} results={this.state.currentResults} /> : null}
+                    </div>
+
+                </div>
             </>
         );
-  }
+    }
 }
 
 export default withRouter(Navbar);
