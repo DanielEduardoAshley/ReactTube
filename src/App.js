bdfae987ecc70f88906a989c26ca5ad7b00ef425
daniel ashley <danielashley@pursuit.org>
2019-03-02 14:13:21 -0500

    removed a space

--- src/App.js
+++ src/App.js
@@ -39,101 +39,43 @@ class App extends Component {
   // Route for app /home
     return (
       <>
-<<<<<<< HEAD
- 
-
-{/* <Route path='/' exact component={SplashPage}/> */}
-
       
-      
-      {this.props.location.pathname === '/'? <Route path='/' exact component={SplashPage}/> :
-<> <div style={{width:'90%', margin:'0 auto'}}>
-        <div class='top-space'>
-=======
+      {
+        this.props.location.pathname === '/'? 
+        
+        <Route path='/' exact component={SplashPage}/> 
+        
+        :
+
+    <> 
       <div className='backgroundImg'>
-      <div style={{width:'90%', margin:'0 auto'}}>
-        <div className='top-space'>
->>>>>>> e0fe62ed00af5277848a8ed9590c445de08afaf4
-          <div style={{display:'flex', flexWrap:'wrap', height:'42.594px'}}>
-            <div className='navBar'></div>
-            <div className='pageWindow'></div>
-            <div className='sideNavBar'></div>
+        <div style={{width:'90%', margin:'0 auto'}}>
+            <div className='top-space'>
+              <div style={{display:'flex', flexWrap:'wrap', height:'42.594px'}}>
+                <div className='navBar'></div>
+                <div className='pageWindow'></div>
+                <div className='sideNavBar'></div>
+              </div>
+            </div>
+          </div>
+      
+          <div className="App">
+          <Navbar />
+            <div className='pageWindow'>
+                <SearchBar />
+                <Route path='/home' exact component={Home}/>
+                <Route path='/video/:video_id' render={()=><VideoPlayer id={id}/>}/>
+                <Route path='/search/:search' component={Search}/>
+                <Route path='/user' component={User}/>
+                <Route path='/feededitor' component={Feed}/>
+                <Route path='/user/:user_id/history' exact component={ViewHistory}/>
+            </div>
+          <SideNav path={path}/>
           </div>
         </div>
-      </div><div className="App"><Navbar />
-      
-      <div className='pageWindow'>
-          <SearchBar />
-          <Route path='/home' exact component={Home}/>
-          <Route path='/video/:video_id' render={()=><VideoPlayer id={id}/>}/>
-          <Route path='/search/:search' component={Search}/>
-          <Route path='/user' component={User}/>
-          <Route path='/feededitor' component={Feed}/>
-          <Route path='/user/:user_id/history' exact component={ViewHistory}/>
-      </div>
-      <SideNav />
-      
-      </div>
-      
       </>
 } 
-     
-     
-      
-<<<<<<< HEAD
-=======
-        <SideNav path={path}/>
-               
-        {/* 
-        Pam's workspace
-         */}
 
-        {/* 
-        Daniel's workspace
-        
-         */}
-
-        {
-          this.state.videoArray.map((e,i)=>{
-            console.log('this is e', typeof e)
-            const id = e
-            return <div key={i}>{this.VideoPlayer({ id })}</div>
-
->>>>>>> e0fe62ed00af5277848a8ed9590c445de08afaf4
-
-
-
-      {/* <div style={{width:'90%', margin:'0 auto'}}>
-        <div class='top-space'>
-          <div style={{display:'flex', flexWrap:'wrap', height:'42.594px'}}>
-            <div className='navBar'></div>
-            <div className='pageWindow'></div>
-            <div className='sideNavBar'></div>
-          </div>
-        </div>
-      </div>
-      
-      <div className="App">
-      
-      <Navbar />
-      
-      <div className='pageWindow'>
-          <SearchBar />
-          <Route path='/home' exact component={Home}/>
-          <Route path='/video/:video_id' render={()=><VideoPlayer id={id}/>}/>
-          <Route path='/search/:search' component={Search}/>
-          <Route path='/user' component={User}/>
-          <Route path='/feededitor' component={Feed}/>
-          <Route path='/user/:user_id/history' exact component={ViewHistory}/>
-      </div>
-<<<<<<< HEAD
-      <SideNav />
-     
-      </div> */}
-
-=======
-      </div>
->>>>>>> e0fe62ed00af5277848a8ed9590c445de08afaf4
       </>
     );
   }
