229b511a1ee8e337cc0854bdd5ad391d2b093adf
daniel ashley <danielashley@pursuit.org>
2019-03-02 01:30:00 -0500

    Made splashPage

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Feed from './components/feed/feed';
 import ViewHistory from './components/viewHistory/viewHistory';
 import SearchBar from './components/searchBar/searchBar';
 import SideNav from './components/sideNav/sideNav';
+import SplashPage from './components/splashpage/splashpage';
 
 // extra import here
 // extra import here
@@ -33,9 +34,50 @@ class App extends Component {
   const p= path.split('/')
   console.log(p[2])
   const id = p[2]
+
+  // ROute for splash page
+  // Route for app /home
     return (
       <>
-      <div style={{width:'90%', margin:'0 auto'}}>
+ 
+
+{/* <Route path='/' exact component={SplashPage}/> */}
+
+      
+      
+      {this.props.location.pathname === '/'? <Route path='/' exact component={SplashPage}/> :
+<> <div style={{width:'90%', margin:'0 auto'}}>
+        <div class='top-space'>
+          <div style={{display:'flex', flexWrap:'wrap', height:'42.594px'}}>
+            <div className='navBar'></div>
+            <div className='pageWindow'></div>
+            <div className='sideNavBar'></div>
+          </div>
+        </div>
+      </div><div className="App"><Navbar />
+      
+      <div className='pageWindow'>
+          <SearchBar />
+          <Route path='/home' exact component={Home}/>
+          <Route path='/video/:video_id' render={()=><VideoPlayer id={id}/>}/>
+          <Route path='/search/:search' component={Search}/>
+          <Route path='/user' component={User}/>
+          <Route path='/feededitor' component={Feed}/>
+          <Route path='/user/:user_id/history' exact component={ViewHistory}/>
+      </div>
+      <SideNav />
+      
+      </div>
+      
+      </>
+} 
+     
+     
+      
+
+
+
+      {/* <div style={{width:'90%', margin:'0 auto'}}>
         <div class='top-space'>
           <div style={{display:'flex', flexWrap:'wrap', height:'42.594px'}}>
             <div className='navBar'></div>
@@ -46,12 +88,11 @@ class App extends Component {
       </div>
       
       <div className="App">
+      
       <Navbar />
       
       <div className='pageWindow'>
           <SearchBar />
-      
-          <Route path='/' exact component={Home}/>
           <Route path='/home' exact component={Home}/>
           <Route path='/video/:video_id' render={()=><VideoPlayer id={id}/>}/>
           <Route path='/search/:search' component={Search}/>
@@ -59,38 +100,9 @@ class App extends Component {
           <Route path='/feededitor' component={Feed}/>
           <Route path='/user/:user_id/history' exact component={ViewHistory}/>
       </div>
-
-      
-        <SideNav />
-               
-        {/* 
-        Pam's workspace
-         */}
-
-        {/* 
-        Daniel's workspace
-        
-         */}
-
-        {
-          this.state.videoArray.map((e,i)=>{
-            console.log('this is e', typeof e)
-            const id = e
-            return <div key={i}>{this.VideoPlayer({ id })}</div>
-
-
-          })
-        }
-
-        {/*         
-        Yun's workspace
-         */}
-        
-        {/*         
-        Rupa's workspace
-          */}
-
-      </div>
+      <SideNav />
+     
+      </div> */}
 
       </>
     );
--- src/services/axios.js
+++ src/services/axios.js
@@ -15,12 +15,12 @@ import axios from 'axios'
         type: 'video',
         videoEmbeddable: 'true',
 
-        // key:'AIzaSyAp4QI_v1IXVKwmBfnij-X69jnKQkUv-GE',
+        key:'AIzaSyAp4QI_v1IXVKwmBfnij-X69jnKQkUv-GE',
         // key:"AIzaSyC55OjTm-LHRPginNybpzjCClbJELrIeYo",
         // key: 'AIzaSyDk4Baz4ZsCIIY-zwzjEgOATbmVwjZVVpc',
         // key:'AIzaSyDRq-1-Gog-yzzF5buaQRPHv3IysLOZWNI',
         // key: 'AIzaSyAWWomrb5NRZn7oTqjszqm6f2uYqW39cvc',
-        key: 'AIzaSyAq6KMp_lshvjdlu044KvIgax3VAMa023g',
+        // key: 'AIzaSyAq6KMp_lshvjdlu044KvIgax3VAMa023g',
         // key: 'AIzaSyCcZMHijbdSvcuTHdRGKWrwsN3j1t0-hoE',
         q: `${query}`,
         pageToken: `${nextPageToken}`
