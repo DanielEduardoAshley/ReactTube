234353d156411e340a56dd6557b147de1d0a12e1
daniel ashley <danielashley@pursuit.org>
2019-03-02 14:20:01 -0500

    changed code stuff

--- src/components/splashpage/splashpage.js
+++ src/components/splashpage/splashpage.js
@@ -25,7 +25,7 @@ render(){
     <source src="https://player.vimeo.com/external/158148793.hd.mp4?s=8e8741dbee251d5c35a759718d4b0976fbf38b6f&profile_id=119&oauth2_token_id=57447761" type="video/mp4" />
     {/* <source src="http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4" type="video/ogg" /> */}
 </video>
-    <div class="content">
+    <div className="content">
       <h4>Pyrd</h4>
       <p>Our Latest Social Video Player</p>
       <button id="myBtn" onClick={this.myFunctionPlay.bind(this)}>Play</button>
