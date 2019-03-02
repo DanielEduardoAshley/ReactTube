6d8857ac69c978658f52e784f06e4b18643e83ba
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-02 01:41:56 -0500

    updates the EXPLORE title when clicking on different routes

--- src/components/sideNav/sideNav.js
+++ src/components/sideNav/sideNav.js
@@ -10,30 +10,18 @@ class SideNav extends Component {
   }
 
   componentDidMount () {
-    const activeUserLS = JSON.parse(localStorage.getItem('activeUser')) || {
-      name: 'Default',
-      feedList: ["Naruto", "Itachi", "Kiki Do U Love Me", "Jiraiya", "Naruto Love Story"]
-    }
 
-    // this.setState({activeName: activeUserLS.name, feedlist: activeUserLS.feedList })
-    // const routeNameLS = localStorage.getItem('routeName')
-    // console.log('Side nav',this.props)
-    // console.log('here in side NAV:', routeNameLS)
-    // console.log('In the side Nav',this.props)
-    // console.log('This is location',this.props.location.pathname)
-    // console.log('This is match info object',this.props.match.params)
-    // console.log('This is location',this.props.location.pathname)
-    // this.setState({routeName: routeNameLS})
   }
 
   componentDidUpdate (prevProps) {
-    console.log('this is props', this.props.path)
     if (this.props.path.toLowerCase() !== prevProps.path) {
       
       const currentRouteArr = this.props.path.split('/');
       const currentRouteName = currentRouteArr[1];
 
-      if(currentRouteName === '' || currentRouteName === 'home'){
+      if(currentRouteName.toLowerCase() === 'feededitor'){
+        this.setState({routeName: 'Feed Editor'})
+      } else if(currentRouteName === '' || currentRouteName === 'home'){
         this.setState({routeName: 'Explore'})
       } else {
         this.setState({routeName: currentRouteName})
