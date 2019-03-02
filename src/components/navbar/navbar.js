e724b578753614606978f3ce4db772e06e1af1ed
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-02 01:41:33 -0500

    updated navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,18 +2,15 @@ import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 class Navbar extends Component {
-    onClickRoute (routeName) {
-        localStorage.setItem('routeName', routeName)
-    }
     render() {
         return (
             <>
                 <div className='navBar' >
                     <h1 className="logo">PRYD</h1>
                     <div className='navBarContainer'>
-                        <Link to='/home' className="navBarButton" onClick={() => this.onClickRoute('home')}>Home</Link>
-                        <Link to='/user' className="navBarButton" onClick={() => this.onClickRoute('user')}>User</Link>
-                        <Link to='/feededitor' className="navBarButton" onClick={() => this.onClickRoute('feed editor')}>Feed Editor</Link>
+                        <Link to='/home' className="navBarButton" >Home</Link>
+                        <Link to='/user' className="navBarButton" >User</Link>
+                        <Link to='/feededitor' className="navBarButton" >Feed Editor</Link>
                     </div>
                 </div>
             </>
