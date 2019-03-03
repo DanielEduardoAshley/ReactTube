e4fe6d7571fee4c13c03c5406906366e08ce0655
daniel ashley <danielashley@pursuit.org>
2019-03-02 23:33:47 -0500

    Updates

---
+++ precache-manifest.a0f2e73e03f920776093457b13001f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53f750fc647d09ea9589cbf39a725825",
+    "url": "/ReactTube/static/media/Triangles-1s-200px.53f750fc.gif"
+  },
+  {
+    "revision": "6a279e063870394b8c4b",
+    "url": "/ReactTube/static/js/runtime~main.6a279e06.js"
+  },
+  {
+    "revision": "9b780ea042004080f70f",
+    "url": "/ReactTube/static/js/main.9b780ea0.chunk.js"
+  },
+  {
+    "revision": "c1913e9f32ebb68ff42f",
+    "url": "/ReactTube/static/js/2.c1913e9f.chunk.js"
+  },
+  {
+    "revision": "9b780ea042004080f70f",
+    "url": "/ReactTube/static/css/main.8ac59ee9.chunk.css"
+  },
+  {
+    "revision": "7c92266fab47b1d8b342cfb2053d078b",
+    "url": "/ReactTube/index.html"
+  }
+];
